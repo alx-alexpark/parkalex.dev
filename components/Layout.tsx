9f9515c10f1e02c4dda37a5eb5b638dd5e0a0b52
alx-alexpark <parkalex380@gmail.com>
2022-03-29 19:09:13 +0000

    contact page wip

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,26 +16,18 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
     <>
       <Head customMeta={customMeta} />
       <header>
-        <div className="max-w-5xl px-8 mx-auto">
-          <div className="flex items-center justify-between py-6">
+        <div className="max-w-5xl px-8 py-6 mx-auto">
+          <div className="flex items-center justify-between">
             <Navigation />
             <ThemeSwitch />
           </div>
         </div>
       </header>
       <main>
-        <div className="max-w-5xl px-8 py-4 mx-auto">{children}</div>
+        <div className="max-w-5xl px-8 mx-auto">{children}</div>
       </main>
       <footer className="py-8">
-        <div className="max-w-5xl px-8 mx-auto">
-          Built by{' '}
-          <a
-            className="text-gray-900 dark:text-white"
-            href="https://twitter.com/huntarosan"
-          >
-            Hunter Chang
-          </a>
-        </div>
+        <div className="max-w-5xl px-8 mx-auto"></div>
       </footer>
     </>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -13,6 +13,23 @@ export const About = (): JSX.Element => {
         If for some random reason you want to contact me then you are at the
         right place
       </p>
+      <p>
+        <strong>
+          If you are to contact me through an insecure medium (SMS, Email),
+          encrypt your messages with my GPG Key
+        </strong>
+      </p>
+      <p>
+        Pull it from a{' '}
+        <a href="https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x69ebcbf0a7d62959881b4a2c24a3b83fb26aa900">
+          Keyserver
+        </a>
+      </p>
+      <code>
+        gpg --keyserver keyserver.ubuntu.com --recv-keys 24a3b83fb26aa900
+      </code>
+      <br /> <br />
+      <code>Email: public@funny-bunny.ninja</code>
     </Layout>
   );
 };
