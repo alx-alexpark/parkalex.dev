d9ef91f4f65101333c921aa85c0c7b1f6c6308a8
alx-alexpark <parkalex380@gmail.com>
2022-03-30 14:19:54 +0000

    change meta tags of website

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,15 +3,13 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { MetaProps } from '../types/layout';
 
-export const WEBSITE_HOST_URL = 'https://nextjs-typescript-mdx-blog.vercel.app';
+export const WEBSITE_HOST_URL = 'https://parkalex.tech';
 
 const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
   const router = useRouter();
   const meta: MetaProps = {
-    title: 'Hunter Chang - Website',
-    description:
-      'Sleep Deprived Father. Senior Web Developer. Lover of all things Ramen and Kpop.',
-    image: `${WEBSITE_HOST_URL}/images/site-preview.png`,
+    title: 'Alex Park - Website',
+    description: 'Its me Alex Park',
     type: 'website',
     ...customMeta,
   };
@@ -23,15 +21,11 @@ const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
       <meta property="og:url" content={`${WEBSITE_HOST_URL}${router.asPath}`} />
       <link rel="canonical" href={`${WEBSITE_HOST_URL}${router.asPath}`} />
       <meta property="og:type" content={meta.type} />
-      <meta property="og:site_name" content="Hunter Chang - Website" />
+      <meta property="og:site_name" content="Alex Park - My website" />
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@huntarosan" />
-      <meta name="twitter:title" content={meta.title} />
-      <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
+
       {meta.date && (
         <meta property="article:published_time" content={meta.date} />
       )}
