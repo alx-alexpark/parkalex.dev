ad16eb50ea092ac938b5efda594d9987302fe211
Alexander Park <parkalex380@gmail.com>
2022-04-02 21:06:47 -0500

    bruh i confused margin and padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,26 +29,28 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         </div>
       </Flex>
 
-      {posts.map((post) => (
-        <article key={post.slug} className="mt-12">
-          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-            {format(parseISO(post.date), 'MMMM dd, yyyy')}
-          </p>
-          <h1 className="mb-2 text-xl">
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
-                {post.title}
-              </a>
-            </Link>
-          </h1>
-          <p className="mb-3">{post.description}</p>
-          <p>
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a>Read More</a>
-            </Link>
-          </p>
-        </article>
-      ))}
+      <div className={styles.indexbody}>
+        {posts.map((post) => (
+          <article key={post.slug} className="mt-12" color="white">
+            <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+              {format(parseISO(post.date), 'MMMM dd, yyyy')}
+            </p>
+            <h1 className="mb-2 text-xl">
+              <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+                <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
+                  {post.title}
+                </a>
+              </Link>
+            </h1>
+            <p className="mb-3">{post.description}</p>
+            <p>
+              <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+                <a>Read More</a>
+              </Link>
+            </p>
+          </article>
+        ))}
+      </div>
     </Layout>
   );
 };
