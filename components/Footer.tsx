0eca55ecc92a3656abbb6be66e140768107c5c9f
Alexander Park <me@parkalex.dev>
2023-05-01 17:10:07 -0500

    bump version for the first time in a really long time

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = (): JSX.Element => {
     return (
         <Flex backgroundColor="white" width="100vw" height="7.5vh" justifyContent="space-between" alignItems="center" paddingLeft="1em" paddingRight="1em" marginTop="3em">
-            <Text fontFamily="Jetbrains Mono" fontSize="1em">Alexander Park | v2.1.1</Text>
+            <Text fontFamily="Jetbrains Mono" fontSize="1em">Alexander Park | v2.2.0</Text>
             <Text fontFamily="Jetbrains Mono" fontSize="1em"><Link href="https://github.com/alx-alexpark/parkalex.dev"><u>Source Code</u></Link></Text>
         </Flex>
     );
